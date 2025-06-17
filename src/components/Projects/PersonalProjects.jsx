37d7687d96619e7a3dc671e605ae46179398959e
Sunil G <gsunil99910@gmail.com>
2025-06-18 00:28:28 +0530

    personal projects added

--- src/components/Projects/PersonalProjects.jsx
+++ src/components/Projects/PersonalProjects.jsx
@@ -1,9 +1,9 @@
-import { profeesionalProjects } from "../../constants";
+import { personalProjects } from "../../constants";
 import Work from "../Work/Work";
 
 
 const PersonalProjects = () => {
-  return <Work projects={profeesionalProjects} title="Personal Projects" description="Projects I've built independently to explore new ideas, sharpen my skills, and bring concepts to life." /> 
+  return <Work projects={personalProjects} title="Personal Projects" description="Projects I've built independently to explore new ideas, sharpen my skills, and bring concepts to life." /> 
 };
 
 export default PersonalProjects;
