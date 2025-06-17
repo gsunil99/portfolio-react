7e903f1a15359ed4cad79436a53095a57aef52e2
Sunil G <gsunil99910@gmail.com>
2025-06-17 15:02:25 +0530

    professional projects added

---
+++ src/components/Projects/ProfessionalProjects.jsx
@@ -0,0 +1,9 @@
+import { profeesionalProjects } from "../../constants";
+import Work from "../Work/Work";
+
+
+const ProfessionalProjects = () => {
+  return <Work projects={profeesionalProjects} title="PROFESSIONAL PROJECTS" description="A showcase of my professional projects and contributions across various organizations." /> 
+};
+
+export default ProfessionalProjects;
